19a12f9748ab9e1488bd2ed83158e80bfceabf07
masato-github <masato.tanaka@di-v.co.jp>
2019-06-21 14:59:11 +0900

    add-reviews-new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,23 @@
 class ReviewsController < ApplicationController
   def index
-    
+    @reviews = Review.find(1)
   end
-end
+
+  def new
+  end
+
+  def create
+    @review = Review.new(permit_params)
+    @review.save!
+    redirect_to action: 'index'
+  end
+
+
+  #プライベート
+  private
+    def permit_params
+      params.permit(:title, :image, :text)
+    end
+
+
+  end
