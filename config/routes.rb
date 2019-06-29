46b1dd832b43bab8941ef97b8afb69d9db52dae3
masato-github <masato.tanaka@di-v.co.jp>
2019-06-30 01:48:36 +0900

    Revert "delete-commit"
    This reverts commit 39dc9daba6cc2f1f456ae92da516e2a77c204751.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # いいね機能 as: で prefix名　を指定できます
+  post   '/like/:review_id' => 'likes#like',   as: 'like'
+  delete '/like/:review_id' => 'likes#unlike', as: 'unlike'
+
   root    'reviews#index'
   resources :reviews
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
-  # いいね機能 as: で prefix名　を指定できます
-  post   '/like/:review_id' => 'likes#like',   as: 'like'
-  delete '/like/:review_id' => 'likes#unlike', as: 'unlike'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_011019) do
+ActiveRecord::Schema.define(version: 2019_06_24_160858) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2019_06_28_011019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count"
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
